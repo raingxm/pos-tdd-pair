b657d00ba4c5dba79ebe08a1443e1a3a09c28ae0
zhangxv <raingxm@163.com>
2014-11-13 19:42:38 +0800

    finish test, can populate items properly

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,3 +1,25 @@
 function printInventory(inputs) {
-  
+
+}
+
+function populateInputs(inputs) {
+  var goodMap = {};
+  var barcode = "";
+  var amount = 0;
+  for(var i = 0; i < inputs.length; i++) {
+    if(inputs[i].indexOf('-') >= 0) {
+      barcode = inputs[i].split('-')[0];
+      amount = parseInt(inputs[i].split('-')[1]);
+      goodMap[barcode] = amount;
+      continue;
+    }
+
+    barcode = inputs[i];
+    if(goodMap[barcode]) {
+      goodMap[barcode]++;
+    }else {
+      goodMap[barcode] = 1;
+    }
+  }
+  return goodMap;
 }
